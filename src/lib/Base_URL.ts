0389023f32468693d99bf6b64a4d9586f4510b12
Muhammad Ranju <mdranju23@gmail.com>
2025-08-29 03:08:27 +0600

    feat: update base url with real Actual workable live backend URL

--- src/lib/Base_URL.ts
+++ src/lib/Base_URL.ts
@@ -1,1 +1,3 @@
-export const BASE_URL = "http://10.10.7.101:5000/api/v1";
+export const BASE_URL =
+  "https://digital-wallet-api-in-socketio.onrender.com/api/v1";
+// export const BASE_URL = "http://10.10.7.101:5000/api/v1";
